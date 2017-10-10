9dcecd2a1277763b8cccf821549f80660158f03a
DLeirer <k1184970@kcl.ac.uk>
2017-10-10 19:23:08 +0100

    Update To CellMix

---
+++ R/P0_CellMix_GAP_FEP.R
@@ -0,0 +1,33 @@
+
+#CellMix approach:
+
+
+
+# Install and load Libraries: ---------------------------
+
+
+library(CellMix)
+library(lumi)
+library(reshape)
+library(plyr)
+library(dplyr)
+library(ggplot2)
+
+
+
+
+
+# Set directories: ---------------------------
+
+setwd("/home/daniel/Documents/A_Year_4_PHD")
+getwd()
+
+data_dir <-"./data/"
+P0_output_dir <-"./P0_Characterise/output/"
+P0_figs_dir <-"./P0_Characterise/figs/"
+
+
+
+# S: ---------------------------
+
+
