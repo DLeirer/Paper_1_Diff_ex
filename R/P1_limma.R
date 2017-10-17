952678328f96b8fe39ec114b49540a0888a51fbc
Daniel Leirer <k1184970@kcl.ac.uk>
2017-10-17 18:45:17 +0100

    Limma done

--- R/P1_limma.R
+++ R/P1_limma.R
@@ -47,11 +47,35 @@ Limma_Datatoplot_fun <- function(limma_matrix){
   return(small_matrix %>% filter(PROBE_KEEP=="KEEP")) 
 }
 
+
+
+#Save results in TSV
+save_limma_results_fun<-function(limma_df,analysis_name){
+  
+  ##All probes
+  write.table(limma_df, file=paste(P1_output_dir,analysis_name,"_all_probes_limma_results.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
+  
+  #Make smaller and clean
+  limma_df_small<-filter(limma_df,Sig_LogFC_probes == "Diffexprs")[,c("TargetID","CHROMOSOME","DEFINITION","logFC","adj.P.Val")]
+  limma_df_small$logFC<-signif(limma_df_small$logFC,2)
+  limma_df_small$adj.P.Val<-signif(limma_df_small$adj.P.Val,2)
+  
+  ##upregulated
+  table_sig_up<-limma_df_small[limma_df_small$logFC >= 0.1,]
+  write.table(table_sig_up, file=paste(P1_output_dir,analysis_name,"_upregulated_significant_limma_results.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
+  
+  
+  ##downregulated
+  table_sig_down<-limma_df_small[limma_df_small$logFC <= -0.1,]
+  write.table(table_sig_down, file=paste(P1_output_dir,analysis_name,"_downregulated_significant_limma_results.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
+}
+
+
 #Volcano Plot function
-Volcano_plot_fun <- function(datatoplot,n_up_probes=10,n_down_probes=10,n_sig_probes = 20){
+Volcano_plot_fun <- function(datatoplot,title_comp,n_up_probes=10,n_down_probes=10,n_sig_probes = 20){
   x_max = max(datatoplot$logFC)+0.5
   x_min = min(datatoplot$logFC)-0.5
-  with(datatoplot, plot(logFC, -log10(P.Value), pch=20, main="Volcano Plot of Differentially Expressed Probes", xlim=c(x_min,x_max)))
+  with(datatoplot, plot(logFC, -log10(P.Value), pch=20, main=paste("Volcano Plot of",title_comp,"comparison",sep=" "), xlim=c(x_min,x_max)))
   
   # Add colored points: red if padj<0.05, orange of log2FC>1, green if both)
   with(subset(datatoplot, logFC >= 0 ), points(logFC, -log10(P.Value), pch=20, col="navy"))
@@ -68,6 +92,19 @@ Volcano_plot_fun <- function(datatoplot,n_up_probes=10,n_down_probes=10,n_sig_pr
   with(datatoplot[datatoplot$TargetID%in%Probes_plot,], textxy(logFC, -log10(P.Value), labs=TargetID, cex=.5))
 }
 
+#Chromosome plot function
+Chromosome_plot_fun<-function(data_limma,title,logFC_up= 0.3,logFC_down = -0.27){
+  ggplot(data = data_limma, 
+         aes(x = CHROMOSOME, y=logFC, color=CHROMOSOME)) +
+    geom_boxplot(alpha = 0)+
+    geom_text(data=filter(data_limma, logFC >= logFC_up |logFC <= logFC_down),check_overlap = TRUE,angle=45,
+              aes(CHROMOSOME,logFC,label=TargetID),size=3)+
+    ggtitle(title)+ 
+    theme_bw(base_size = 10) + 
+    theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 0, hjust = 1))
+  
+}
+
 
 # Set directories ---------------------------------------------------------
 
@@ -81,10 +118,6 @@ P1_figs_dir <-"./P1_Diff_Ex/figs/"
 # Load eset ---------------------------------------------------------------
 
 load(paste(data_dir,"GAP_FEP_small_Gene_Expression_Data.RData",sep=""))
-
-
-
-# Linear model to remove Age Ethnicity Sex and Blood differences: ---------
 
 
 # Limma case control ------------------------------------------------------
@@ -106,153 +139,38 @@ contrast.fit <- contrasts.fit(fit, contrasts)
 contrast.fit <- eBayes(contrast.fit)
 
 
-# Top Table ---------------------------------------------------------------
-
-top_de_genes <- topTable(contrast.fit, coef=1, number=5000,adjust.method="fdr",p.value=1,confint=TRUE)
-
-head(top_de_genes)
+# Case Control outputs (tables and figures) ---------------------------------------------------------------
 
 
+analysis_name<-"FEP vs Control"
 
-# tidy up
-top_de_genes$logFC <- round(top_de_genes$logFC,2)
-top_de_genes$CI.L <- round(top_de_genes$CI.L,2)
-top_de_genes$CI.R <- round(top_de_genes$CI.R,2)
-top_de_genes$AveExpr <- round(top_de_genes$AveExpr,2)
-top_de_genes$t <- round(top_de_genes$t,2)
-top_de_genes$P.Value <- signif(top_de_genes$P.Value,3)
-top_de_genes$adj.P.Val <- signif(top_de_genes$adj.P.Val,3)
-top_de_genes$B <- round(top_de_genes$B,2)
+#Data and save
+FEP_Con_TopGenes <- topTable(contrast.fit, coef=1, number=5000,adjust.method="fdr",p.value=1,confint=TRUE)
+FEP_Con_TopGenes<-Limma_Datatoplot_fun(FEP_Con_TopGenes)
 
 
 
-
-##Save full dataframe
-full_limma_results_AgeSexEthnicity_Adj <- top_de_genes
-write.table(full_limma_results_AgeSexEthnicity_Adj, file=paste(P1_output_dir,"full_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-##Save Reduced dataframe
-small_limma_results_AgeSexEthnicity_Adj <- top_de_genes[,c("TargetID","logFC","P.Value","adj.P.Val","CHROMOSOME","DEFINITION")]
-write.table(small_limma_results_AgeSexEthnicity_Adj, file=paste(P1_output_dir,"small_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
+#Save Limma Results up down and full.
+save_limma_results_fun(FEP_Con_TopGenes,analysis_name)
 
 
-
-# Significantly DE Probes -------------------------------------------------
-
-de_res <- top_de_genes[,c("TargetID","logFC","P.Value","adj.P.Val","CHROMOSOME","DEFINITION")]
-
-de_res <- de_res %>% 
-  mutate(SIG_DE=adj.P.Val <=0.05, 
-         LogFC_DIRECTION=ifelse(logFC >= 0, "up-regulated",
-                                ifelse(logFC < 0, "down-regulated", "no-change")),
-         LogFC_BIOLOCICAL=ifelse(logFC >= 0.1, "up-regulated",
-                                 ifelse(logFC <= -0.1, "down-regulated", "no-sig-change")),
-         PROBE_KEEP=ifelse( grepl("^LOC",TargetID),"DROP",
-                            ifelse( grepl("^HS\\.",TargetID), "DROP","KEEP"))
-  ) %>% mutate(Sig_LogFC_probes = ifelse(SIG_DE==TRUE & LogFC_BIOLOCICAL != "no-sig-change","Diffexprs","BACKGROUND"))
-
-
-## Significantly DE Probes
-datatoplot<- de_res %>% filter(PROBE_KEEP=="KEEP")
-
-write.table(datatoplot, file=paste(P1_output_dir,"Datatoplot.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-
-## Number of probes
-dim(de_res)
-dim(datatoplot)
-table(datatoplot$PROBE_KEEP)
-4730-4063
-
-## Number of significant probes
-table(datatoplot$Sig_LogFC_probes)
-
-
-##All probes
-write.table(de_res, file=paste(P1_output_dir,"Supplementary_Table_1_all_probes_4730_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-#write.table(datatoplot, file=paste(P1_output_dir,"Supplementary_Table_2_all_probes_4063_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-##Significant probes
-#datatoplot_sig<-datatoplot[datatoplot$adj.P.Val <= 0.05,]
-#datatoplot_sig<-datatoplot[datatoplot$LogFC_BIOLOCICAL != "no-sig-change" & datatoplot$SIG_DE == TRUE,]
-datatoplot_sig<-datatoplot[datatoplot$Sig_LogFC_probes == "Diffexprs",]
-dim(datatoplot_sig)
-write.table(datatoplot_sig, file=paste(P1_output_dir,"Supplementary_Table_2_significant_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-
-
-
-##upregulated
-datatoplot_sig_up<-datatoplot_sig[datatoplot_sig$logFC >= 0.1,]
-dim(datatoplot_sig_up)
-write.table(filter(datatoplot_sig_up,Sig_LogFC_probes == "Diffexprs"), file=paste(P1_output_dir,"Supplementary_Table_3_upregulated_significant_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-table1up<-filter(datatoplot_sig_up,Sig_LogFC_probes == "Diffexprs")[1:50,c("TargetID","CHROMOSOME","DEFINITION","logFC","adj.P.Val")]
-
-
-table1up$logFC<-signif(table1up$logFC,2)
-table1up$adj.P.Val<-signif(table1up$adj.P.Val,2)
-
-write.table(table1up, file=paste(P1_output_dir,"Table_1_upregulated_significant_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-
-##downregulated
-datatoplot_sig_down<-datatoplot_sig[datatoplot_sig$logFC <= -0.1,]
-dim(datatoplot_sig_down)
-write.table(datatoplot_sig_down, file=paste(P1_output_dir,"Supplementary_Table_5_downregulated_significant_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-dim(filter(datatoplot_sig_down,Sig_LogFC_probes == "Diffexprs"))
-
-table2down<-filter(datatoplot_sig_down,Sig_LogFC_probes == "Diffexprs")[1:50,c("TargetID","CHROMOSOME","DEFINITION","logFC","adj.P.Val")]
-
-table2down$logFC<-signif(table2down$logFC,2)
-table2down$adj.P.Val<-signif(table2down$adj.P.Val,2)
-
-write.table(table2down, file=paste(P1_output_dir,"Table_2_downregulated_significant_limma_results_AgeSexEthnicity_Adj.tsv",sep=""),row.names=FALSE,quote=FALSE,sep = "\t")
-
-
-
-
-# Plots: Volcano and Chromosome -------------------------------------------
-
-# Make a basic volcano plot
-
-filename <- "Volcanoplot.jpeg"
+## Plots
+#Volcano Plot
+filename <- paste(analysis_name,"_Volcanoplot.jpeg",sep="")
 jpeg(file = paste(P1_figs_dir,filename,sep=""), pointsize = 20, width = 1500, height = 1300)
-with(datatoplot, plot(logFC, -log10(P.Value), pch=20, main="Volcano Plot of Differentially Expressed Probes", xlim=c(-1,1)))
-
-# Add colored points: red if padj<0.05, orange of log2FC>1, green if both)
-with(subset(datatoplot, logFC >= 0 ), points(logFC, -log10(P.Value), pch=20, col="navy"))
-with(subset(datatoplot, logFC <= 0 ), points(logFC, -log10(P.Value), pch=20, col="darkgreen"))
-with(subset(datatoplot, Sig_LogFC_probes =="BACKGROUND" ), points(logFC, -log10(P.Value), pch=20, col="firebrick"))
-
-
-# Label points with the textxy function from the calibrate plot
-with(datatoplot [c(1,4,8,9,10,12),], textxy(logFC, -log10(P.Value), labs=TargetID, cex=.5))
-with(subset(datatoplot, logFC < -0.25 & adj.P.Val <0.002 ), textxy(logFC, -log10(P.Value), labs=TargetID, cex=.5,offset = .6))
-with(subset(datatoplot, logFC < -0.33 & adj.P.Val <0.05 ), textxy(logFC, -log10(P.Value), labs=TargetID, cex=.5,offset = .6))
-with(subset(datatoplot, logFC >= 0.38 & adj.P.Val <0.05 ), textxy(logFC, -log10(P.Value), labs=TargetID, cex=.5,offset = .6))
+Volcano_plot_fun(FEP_Con_TopGenes,analysis_name,10,10,10)
 dev.off()
 
 
-#LogFC by chromosome
-title = "LogFC by Chromosome"
-ggplot(data = datatoplot, 
-       aes(x = CHROMOSOME, y=logFC, color=CHROMOSOME)) +
-  geom_boxplot(alpha = 0)+
-  geom_text(data=filter(datatoplot, logFC >= 0.3 |logFC <= -0.27),check_overlap = TRUE,angle=45,
-            aes(CHROMOSOME,logFC,label=TargetID),size=3)+
-  ggtitle(title)+ 
-  theme_bw(base_size = 10) + 
-  theme(plot.title = element_text(hjust = 0.5),axis.text.x = element_text(angle = 0, hjust = 1))
+#Chromosome Plot
+title = paste(analysis_name," LogFC by Chromosome",sep="")
+Chromosome_plot_fun(FEP_Con_TopGenes,title = title)
 ggsave(paste(P1_figs_dir,title,".png",sep=""))
+dev.off()
+  
 
 
-
-
-# Limma Diagnosis ------------------------------------------------------
-
-names(pData_rAESB)
+# Limma Diagnosis 3 way ------------------------------------------------------
 
 #make model Matrix
 design_diagnosis=model.matrix(~0+ICD_DSM+Sex+Age+Ethnicity+Tc+neutro,data=pData_rAESB)
@@ -267,13 +185,6 @@ contrast.matrix <- makeContrasts(ICD_DSMSchizophrenia-ICD_DSMControl,ICD_DSMOthe
 fit_diagnosis2 <- contrasts.fit(fit_diagnosis, contrast.matrix)
 fit_diagnosis2 <- eBayes(fit_diagnosis2)
 
-results <- decideTests(fit_diagnosis2)
-results
-
-VennDia<-"VennDiagram_Compare_diagnosis"
-jpeg(paste(P1_figs_dir,VennDia,".jpeg",sep=""))
-vennDiagram(results,names=c("Scz vs Con", "OP vs Con", "Sch vs OP"),cex=1)
-dev.off()
 
 
 Dia_Scz_Con<-topTable(fit_diagnosis2, coef=1, number=5000,adjust.method="fdr",p.value=1,confint=TRUE)
@@ -288,9 +199,45 @@ top_de_genes <- topTable(contrast.fit, coef=1, number=5000,adjust.method="fdr",p
 Dia_Scz_Con<-Limma_Datatoplot_fun(Dia_Scz_Con)
 head(Dia_Scz_Con)
 
+# diagnosis outputs (tables and figures) ---------------------------------------------------------------
 
+#vennDiagram plot output
+results <- decideTests(fit_diagnosis2)
+results
 
-filename <- "Test.jpeg"
-jpeg(file = paste(P1_figs_dir,filename,sep=""), pointsize = 20, width = 1500, height = 1300)
-Volcano_plot_fun(Dia_Scz_Con)
+VennDia<-"VennDiagram_Compare_diagnosis"
+jpeg(paste(P1_figs_dir,VennDia,".jpeg",sep=""))
+vennDiagram(results,names=c("Scz vs Con", "OP vs Con", "Sch vs OP"),cex=1)
 dev.off()
+
+
+analysis_names<-c("Scz vs Con","OP vs Con","Scz vs OP")
+for (i in 1:length(analysis_names)){
+  print(analysis_names[i])
+  analysis_name_temp<-analysis_names[i]
+  
+  #Generate top list for each comparision and save
+  Top_De_Genes_temp <- topTable(fit_diagnosis2, coef=i, number=5000,adjust.method="fdr",p.value=1,confint=TRUE)
+  
+  Top_De_Genes_temp<-Limma_Datatoplot_fun(Top_De_Genes_temp)
+  #Save Limma Results up down and full.
+  save_limma_results_fun(Top_De_Genes_temp,analysis_name_temp)  
+
+  ## Plots
+  #Volcano Plot
+  filename <- paste(analysis_name_temp,"_Volcanoplot.jpeg",sep="")
+  jpeg(file = paste(P1_figs_dir,filename,sep=""), pointsize = 20, width = 1500, height = 1300)
+  Volcano_plot_fun(Top_De_Genes_temp,analysis_name_temp,10,10,15)
+  dev.off()
+  
+  
+  #Chromosome Plot
+  title = paste(analysis_name_temp," LogFC by Chromosome",sep="")
+  Chromosome_plot_fun(Top_De_Genes_temp,title = title)
+  ggsave(paste(P1_figs_dir,title,".png",sep=""))
+   
+}
+
+
+
+
